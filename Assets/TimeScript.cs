2e0ba83898f6cd2152807b008add9a8cac6113c9
CheezyOP <vc.willemsen@student.avans.nl>
2021-03-13 13:38:30 +0100

    Skills, timer, fixes and more

---
+++ Assets/TimeScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour
+{
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time < 60)
+        {
+            GetComponent<Text>().text = "Time: " + Mathf.RoundToInt(time).ToString() + "s";
+        }
+        else
+        {
+            GetComponent<Text>().text = "Time: " + Mathf.RoundToInt(time / 60).ToString() + "m " + Mathf.RoundToInt(time % 60).ToString() + "s";
+        }
+        
+    }
+}
